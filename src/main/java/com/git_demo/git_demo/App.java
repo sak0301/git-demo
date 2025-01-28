afb5dc422233d782097d01bd017b3d839a812789
Sakshi <sakshiras2107>
2025-01-29 01:11:15 +0530

    first commit

---
+++ src/main/java/com/git_demo/git_demo/App.java
@@ -0,0 +1,18 @@
+package com.git_demo.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void add(String data)
+	{
+		System.out.println("dummy method");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
